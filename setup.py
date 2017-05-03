667824fc9c631e6f6a07f491e64c0ba276b35bc9
Justin Dray ✌️ 🙌 <justin@dray.be>
2017-05-03 16:50:48 +1000

    Include templates in the sdist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="portinus",
-    version="0.9.1",
+    version="0.9.2",
     author="Justin Dray",
     author_email="justin@dray.be",
     url="https://github.com/justin8/portinus",
