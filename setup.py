68682a04435447f37b4d867e20d819bcda1a0409
Justin Dray ✌️ 🙌 <justin@dray.be>
2017-05-03 19:58:50 +1000

    Fix package_data to include templates

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="portinus",
-    version="0.9.2",
+    version="0.9.3",
     author="Justin Dray",
     author_email="justin@dray.be",
     url="https://github.com/justin8/portinus",
     description="This utility creates a systemd service file for a docker-compose file",
     packages=find_packages(),
+    package_data={'portinus': ['templates/*']},
     license="MIT",
     install_requires=[
         "click",
