c5d8c29ce8361d9a81ef7e9954ecef64e9bb0c7e
Justin Dray ✌️ 🙌 <justin@dray.be>
2017-05-04 15:59:06 +1000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="portinus",
-    version="0.9.6",
+    version="0.9.7",
     author="Justin Dray",
     author_email="justin@dray.be",
     url="https://github.com/justin8/portinus",
